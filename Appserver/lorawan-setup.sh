b84509f6c822ad93436e25a893f5b1fa067b424a
daevo <m.roth1991@yahoo.de>
2018-11-15 14:03:46 +0100

    changed lines to comments in lorawan.sh when they weren't gonna be needed in our finished version of the script

--- Appserver/lorawan-setup.sh
+++ Appserver/lorawan-setup.sh
@@ -1,9 +1,9 @@
-cd /home/ubuntu/
-git clone https://github.com/k0nnie/HTWG-Konstanz-LoRaWAN.git
+#cd /home/ubuntu/
+#git clone https://github.com/k0nnie/HTWG-Konstanz-LoRaWAN.git
 sudo apt-get update
 sudo apt-get install python3.6
 sudo apt-get install python3-pip
 pip3 install 'ttn<3'
-cd HTWG-Konstanz-LoRaWAN/Appserver
+#cd HTWG-Konstanz-LoRaWAN/Appserver
 python3 receiver.py #start the script that receives packages from TTN
 #sudo dpkg-reconfigure tzdata  ######select Europe/Berlin#######
