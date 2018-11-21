33299fab476499fb2c0f87074f8d7de0f1b64c8d
daevo <m.roth1991@yahoo.de>
2018-11-21 15:01:57 +0100

    added git ignore and a simple shell script to get data from TTN storage integration

---
+++ Appserver/getTTNdata.sh
@@ -0,0 +1,14 @@
+#gets all data from specified time in $2
+if [ $1 == 'query' ]; then
+	curl -X GET --header 'Accept: application/json' --header 'Authorization: key ttn-account-v2.d7Q2pGiB97SPJFjr_WphmoaZKctmJRE93MhC6T9rP-g' 'https://htwg-konstanz-testapp.data.thethingsnetwork.org/api/v2/query?last='$2
+fi
+
+#lists all deviced of app
+if [ $1 == 'devices' ]; then
+	curl -X GET --header 'Accept: application/json' --header 'Authorization: key ttn-account-v2.d7Q2pGiB97SPJFjr_WphmoaZKctmJRE93MhC6T9rP-g' 'https://htwg-konstanz-testapp.data.thethingsnetwork.org/api/v2/devices'
+fi
+
+#gets all data from specified time in $3 from device $2
+if [ $1 == 'device-id' ]; then
+	curl -X GET --header 'Accept: application/json' --header 'Authorization: key ttn-account-v2.d7Q2pGiB97SPJFjr_WphmoaZKctmJRE93MhC6T9rP-g' 'https://htwg-konstanz-testapp.data.thethingsnetwork.org/api/v2/query/'$2'?last='$3
+fi
