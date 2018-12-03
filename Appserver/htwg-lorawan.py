dffb4ba9f3f3fcd2527a8202aa7dd2d47c57e387
daevo <m.roth1991@yahoo.de>
2018-12-03 16:46:33 +0000

    cleaning up the code

--- Appserver/htwg-lorawan.py
+++ Appserver/htwg-lorawan.py
@@ -9,26 +9,28 @@ import subprocess
 import sys
 #import thread
 
-MicDevices = ["test_mic_0", "test_mic_1"]
-DistanceDevices = ["test_distance_0", "test_distance_1"]
-AllDevices = MicDevices + DistanceDevices
+MICDEVICES = ["test_mic_0", "test_mic_1"]
+DISTANCEDEVICES = ["test_distance_0", "test_distance_1"]
+ALLDEVICES = MICDEVICES + DISTANCEDEVICES
 
+#timedeltas to request the latest data
 LAST30SECONDS = datetime.timedelta(seconds=30)
 LAST5MINUTES = datetime.timedelta(minutes=5)
 LASTHOUR = datetime.timedelta(hours=1)
 LAST2HOURS = datetime.timedelta(hours=2)
 LASTDAY = datetime.timedelta(days=1)
 LASTWEEK = datetime.timedelta(weeks=1)
+ALLTIME = datetime.timedelta(days=99999) #roughly 274 years :)
 
-
+#transforms the time string from our uplink message to datetime
 def timeToDatetime(time):
     tmp = str(time).replace(":" , "-").replace("T" , "-").replace("." , "-")
     tmp = tmp.split("-")
     result = datetime.datetime(int(tmp[0]),int(tmp[1]), int(tmp[2]), int(tmp[3]), int(tmp[4]), int(tmp[5])) #ignoring microseconds to evade possible crashes
-   # print(str(result))
     return result
 
-def getData(time, device): #requests all uplink data from a device that was sent [time] ago.
+#loads data from nodedata file if time and device requirements are met.
+def getData(time, device):
     fullData = []
     resultData = []
     with open ('nodedata', 'r') as f:
@@ -42,14 +44,14 @@ def getData(time, device): #requests all uplink data from a device that was sent
                     resultData.append(line)
     return resultData
 
-
-def filterData(time, devices):
+#allows to filter data to match multiple devices and one timedelta
+def filterData(time, devices): #all data with filterData(ALLTIME, ALLDEVICES)
     data = []
     for device in devices:
         data.append(getData(time, device))
     return data
 
-def transformMicData(data): #creates array from messages, decodes payload, change time format to date:hh:mm:ss, 
+def transformMicData(data):
   pass
 
 def transformDistanceData():
@@ -58,7 +60,7 @@ def transformDistanceData():
 def evaluateData(tMicData, tdistanceData):
   pass
 
-def sendData(resultData): #send finished data or plots
+def sendData(resultData):
   pass #change time to gmt+1
 
 def main():
@@ -66,10 +68,7 @@ def main():
 
     x = getData(LAST2HOURS, "test_distance_0")
     print(str(x))
-   # y = getData(None, 'test_distance_1')
-   # fx = filterData(None, MicDevices)
-   # fy = filterData(None, DistanceDevices)
-   # print(str(fx) + "\n" + str(fy))
+
 if __name__ == "__main__":
   main()
 
