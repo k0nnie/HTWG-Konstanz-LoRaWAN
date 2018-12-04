59d5e3c7ed49bcf7b55838c3bece88c4dc748bb4
Steven Inácio <sthenriq@htwg-konstanz.de>
2018-12-04 14:12:14 +0100

    Added a microphone test that works with the max9814

--- Microphone_test/Microphone_test.ino
+++ Microphone_test/Microphone_test.ino
@@ -1,20 +1,43 @@
-const int soundPin = A1;
+/****************************************
+Example Sound Level Sketch for the 
+Adafruit Microphone Amplifier
+****************************************/
 
-void setup()
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+
+void setup() 
 {
-  Serial.begin(9600);
+   Serial.begin(9600);
 }
 
-void loop()
-{
-  // int value = analogRead(soundPin);
-  int value = analogRead (soundPin) * (5.0 / 1023.0);
-  // Serial.println(value);
-  if (value != 0) {
-    Serial.print ("Analog voltage value:");
-    Serial.print (value, 4);
-    Serial.print ("\n");
-  }
 
-  delay(20);
+void loop() 
+{
+   unsigned long startMillis= millis();  // Start of sample window
+   unsigned int peakToPeak = 0;   // peak-to-peak level
+
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(0);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+   double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
+
+   Serial.println(volts);
 }
